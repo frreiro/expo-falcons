b57a0d3bb4afc287f727ee41f7c678b8045f2ad9
Lucas Viana <lucasvfv@gmail.com>
2023-07-22 16:30:58 +0000

    feat: global styles file added

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,26 @@
+export const GlobalStyles = {
+  colors: {
+    primary: '#292424',
+    secondary: '#E95151',
+    third: '#696262',
+    light: '#fff',
+  },
+  fonts: {
+    family: {
+      hanjet: {
+        regular: 'Handjet-Regular',
+        circleBold: 'HandjetCircle-Bold',
+        circleBlack: 'HandjetCircle-Black',
+        circleExtraLight: 'HandjetCircle-Extralight',
+        circleDoubleLight: 'HandjetCircleDouble-Light',
+        circleDoubleSemibold: 'HandjetCircleDouble-Semibold',
+        circleDoubleThin: 'HandjetCircleDouble-Thin',
+        FlowerDoubleMedium: 'HandjetFlowerDouble-Medium',
+        HeartDoubleExtrabold: 'HandjetHeartDouble-Extrabold',
+      },
+      shareTechMono: {
+        regular: 'ShareTechMono-Regular',
+      },
+    },
+  },
+};
