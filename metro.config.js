50b51b864af2ab6a3b32f0e2fd3da44a4827341a
Lucas Viana <lucasvfv@gmail.com>
2023-07-29 20:38:22 +0000

    fix: metro configurations change for build

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,19 @@
-module.exports = {
-  extends: ' @expo/metro-config',
-  resolver: {
-    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs'],
-    assetExts: ['glb', 'gltf', 'mtl', 'obj', 'png', 'jpg', 'hdr', 'ttf', 'jpg'],
-  },
-};
+const { getDefaultConfig } = require('expo/metro-config');
+
+// eslint-disable-next-line no-undef
+const config = getDefaultConfig(__dirname);
+
+config.resolver.sourceExts.push('js', 'jsx', 'json', 'ts', 'tsx', 'cjs');
+config.resolver.assetExts.push(
+  'glb',
+  'gltf',
+  'mtl',
+  'obj',
+  'png',
+  'jpg',
+  'hdr',
+  'ttf',
+  'jpg',
+);
+
+module.exports = config;
