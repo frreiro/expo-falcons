f421cf0acdc9938ac553da079335e3b017caab6a
Lucas Viana <lucasvfv@gmail.com>
2023-07-29 20:27:06 +0000

    feat: expo eas file added and metro extended expo configs

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  extends: ' @expo/metro-config',
   resolver: {
     sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs'],
     assetExts: ['glb', 'gltf', 'mtl', 'obj', 'png', 'jpg', 'hdr', 'ttf', 'jpg'],
